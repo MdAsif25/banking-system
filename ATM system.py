5e887265cf5604de3d0add2051fabe8806da2c4c
MdAsif <36000940+MdAsif25@users.noreply.github.com>
2019-12-10 07:20:31 +0530

    Add files via upload

---
+++ ATM system.py
@@ -0,0 +1,63 @@
+def main():
+    pinCode = ["1234", "1999", "2424", "1985", "5555"] 
+    accountHoldersName = ["murali", "asif", "niranjan", "gowrishankar", "lokesh"]
+    accountNumber = ['1353', '199281', "182838", "185597", "667432"]
+    balance = [567000, 21873, 2341871, 275638, 91820]
+
+    flag = False
+    for i in range (0,999999999): 
+        print("""\t\t=== Welcome To  ATM System ===""")
+        inputName = input("Enter Your Name: ")
+        inputName = inputName.lower()
+        inputPin = 0000 
+        index = 0 
+        for name in accountHoldersName:
+            count = 0
+            if name == inputName:
+                index = count 
+                inputPin = input("\nEnter Pin Number: ")
+            count += 1
+
+        if inputPin == pinCode[index]:
+            flag = True
+        else:
+            print("Invalid data.")
+            flag = False
+            continue
+        if flag == True:
+            print("\nYour account number is: ",accountNumber[index])
+            print("Your account balance is: Rs.", balance[index])
+            drawOrDeposit = input("\nDo you want to draw or deposit cash (draw/deposite/no): ")
+            if drawOrDeposite == "draw":
+                amount = input("\nEnter the amount you want to draw: ")
+                try:
+                    amount = int(amount)
+                    if amount > balance[index]:
+                        raise
+                except:
+                    print("invalid amount.")
+                    continue
+                remainingBalalnce = balance[index] - amount 
+                balance.remove(balance[index]) 
+                balance.insert(index,remainingBalalnce)
+                availableBalance = print("\nYour available balance is: ",remainingBalalnce)
+            elif drawOrDeposite == "deposite":
+                amount = input("Enter the amount you want to deposit: ")
+                try:
+                    amount = int(amount)
+                    if amount > balance[index]:
+                        raise
+                except:
+                    print("invalid amount.")
+                    continue
+                remainingBalalnce = balance[index] + amount 
+                balance.remove(balance[index])
+                balance.insert(index,remainingBalalnce)
+                availableBalance = print("Your available balance is: ",remainingBalalnce)
+            print("\n\nThank you for using this  ATM System. \n ")
+
+main()
+
+
+
+
